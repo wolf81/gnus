c1bb10cf4fce26dc9ffa7930d8ad2ee4b8fe37b3
Wolfgang Schreurs <w.schreurs@depotsoftware.com>
2025-10-24 19:56:35 +0700

    Update MainLayout.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,7 +3,7 @@ import Background from "./Background"
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative min-h-screen flex justify-center">
-      <div className="relative w-full max-w-[90rem] p-6 rounded-2xl">
+      <div className="relative w-full max-w-[80rem] p-6 rounded-2xl">
         <Background />
         {children}
       </div>
