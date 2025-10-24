a282940920503f77dbd0ac42c520eb90882c8c6e
Wolfgang Schreurs <w.schreurs@depotsoftware.com>
2025-10-24 21:05:21 +0700

    Improve profile cards

--- src/components/ui/ProfileCard.tsx
+++ src/components/ui/ProfileCard.tsx
@@ -5,10 +5,12 @@ interface ProfileCardProps {
     name: string
     role: string,
     image: string
-    children: ReactNode
+    children: ReactNode,
+    linkedIn?: string
+    twitter?: string
 }
 
-export default function ProfileCard({ name, role, image, children }: ProfileCardProps) {
+export default function ProfileCard({ name, role, image, children, twitter, linkedIn }: ProfileCardProps) {
     return (
         <Card className="max-w-md mx-auto p-4">
             <div className="flex items-start gap-4">
@@ -24,8 +26,39 @@ export default function ProfileCard({ name, role, image, children }: ProfileCard
                     <h2 className="font-semibold text-left text-[#FFF]">{name}</h2>
                     <h2 className="text-left text-xs text-muted-foreground">{role}</h2>
                     <div className="flex gap-3 mt-1 text-sm text-muted-foreground">
-                        <a href="#" className="hover:text-blue-500 text-xs">LinkedIn</a>
-                        <a href="#" className="hover:text-blue-500 text-xs">Twitter</a>
+                        {linkedIn && (
+                            <a
+                                href={linkedIn}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="hover:text-blue-500 text-xs"
+                            >
+                                <div className="flex items-baseline gap-2">
+                                    <img
+                                        src="/gnus/linkedin-xs.png"
+                                        alt="LinkedIn"
+                                        className="w-[18px] h-[18px]"
+                                    /> LinkedIn
+
+                                </div>
+                            </a>
+                        )}
+                        {twitter && (
+                            <a
+                                href={twitter}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="hover:text-blue-500 text-xs">
+                                <div className="flex items-baseline gap-2 py-[2px]">
+
+                                    <img
+                                        src="/gnus/x-xs.png"
+                                        alt="X"
+                                        className="w-[18px] h-[18px]"
+                                    />
+                                </div>
+                            </a>
+                        )}
                     </div>
                 </div>
             </div>
--- src/sections/LeadershipTeam.tsx
+++ src/sections/LeadershipTeam.tsx
@@ -10,13 +10,13 @@ export default function LeadershipTeam() {
     <>
       <SectionHeader title="Leadership Team" description="Built by Top Talent from NVIDIA, Google, Amazon, and more. Dedicated to making AI compute affordable and accessible." />
       <div className="flex gap-4 py-10">
-        <ProfileCard name="Kenneth Hurley" role="Founder & CEO" image={hurleyImage}>
+        <ProfileCard name="Kenneth Hurley" role="Founder & CEO" image={hurleyImage} linkedIn='#' twitter='#'>
           Kenneth Hurley has worked at Amazon, Google, NVIDIA, Intel, and EA. With 40+ years in software and gaming, he’s built and scaled systems across platforms.
         </ProfileCard>
-        <ProfileCard name="Michael Hara" role="Business Development" image={haraImage}>
+        <ProfileCard name="Michael Hara" role="Business Development" image={haraImage} linkedIn='#'>
           Michael Hara served 13 years as a Senior Vice President at NVIDIA and brings capital‑markets savvy, strategic leadership, and operational rigor.
         </ProfileCard>
-        <ProfileCard name="George Northup" role="Board Member" image={northrupImage}>
+        <ProfileCard name="George Northup" role="Board Member" image={northrupImage} linkedIn='#'>
           George Northup is a technology entrepreneur and investor with 25 years of leading and advising high‑growth companies.
         </ProfileCard>
       </div>
